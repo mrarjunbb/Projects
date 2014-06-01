4f54ca9660d2cd4859e1efa13f7a046f43e8cb05
root <root@18aa1e570d85.(none)>
2014-06-01 02:39:32 +0000

    copy dcnet subfolder

--- ns3.sh
+++ ns3.sh
@@ -1,4 +1,4 @@
 rm -rf /workspace/bake/source/ns-3.20/scratch/
 mkdir -p /workspace/bake/source/ns-3.20/scratch
-cp /tmp/Projects/* /workspace/bake/source/ns-3.20/scratch/
+cp -R /tmp/Projects/dcnet /workspace/bake/source/ns-3.20/scratch/
 cp /tmp/Projects/*.sh /workspace/bake/source/ns-3.20
